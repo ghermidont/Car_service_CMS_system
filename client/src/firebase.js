7a9bb5824169490f07391967d486a6423d5c2132
ghermidont <mihai_olaru@ymail.com>
2021-06-18 10:43:14 +0300

    first commit

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -2,7 +2,17 @@ import * as firebase from "firebase";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyC0H62pHF1ecsDOim4mdASntlLgS1gzwQ4",
+  apiKey: process.env.REACT_APP_FIRE_BASE_API_KEY,
+     authDomain: process.env.REACT_APP_FIRE_BASE_AUTH_DOMAIN,
+     databaseURL: process.env.REACT_APP_FIRE_BASE_DB_URL,
+     projectId: process.env.REACT_APP_FIRE_BASE_PROJECT_ID,
+     storageBucket: process.env.REACT_APP_FIRE_BASE_STORAGE_BUCKET,
+     messagingSenderId: process.env.REACT_APP_FIRE_BASE_MESSAGING_SENDER_ID,
+     appId: process.env.REACT_APP_FIRE_BASE_APP_ID,
+     measurementId: process.env.REACT_APP_FIRE_BASE_MEASUREMENT_ID,
+     region: 'europe-west1'
+
+    apiKey: "AIzaSyC0H62pHF1ecsDOim4mdASntlLgS1gzwQ4",
     authDomain: "carservicecms.firebaseapp.com",
     projectId: "carservicecms",
     storageBucket: "carservicecms.appspot.com",

1d7b522ab7f1df556b84bd26031857794941ab26
ghermidont <mihai_olaru@ymail.com>
2021-12-14 20:04:34 +0200

    14.12 OFFICE. Implemented all the print tables logic. Working on the car search logic

--- client/src/pages/carsPages/CarsArchivePage.js
+++ client/src/pages/carsPages/CarsArchivePage.js
@@ -229,7 +229,10 @@ export default function CarArchivePage( { history } ) {
                     </div>
                     {/*Search bar START*/}
                     <form className='w-300 flex items-center relative'>
-                        <button className='w-40 h-10 px-2 border border-border rounded-l-full transition hover:opacity-70 focus:opacity-70'>
+                        <button
+                            className='w-40 h-10 px-2 border border-border rounded-l-full transition hover:opacity-70 focus:opacity-70'
+                            //onClick={ handleSubmit }
+                        >
                             <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"> </path>
                             </svg>
